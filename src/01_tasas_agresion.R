3a9177f15c67b28c7c2342ed53fc8e2f8cd96f6d
Songeo <sonia.gmcl@gmail.com>
2017-11-27 00:26:24 -0600

    tasas endireh nuevas tablas y input.l

--- src/01_tasas_agresion.R
+++ src/01_tasas_agresion.R
@@ -4,83 +4,170 @@ library(ProjectTemplate)
 load.project()
 
 source("munge/01_envin.R")
-load("cache/tab_sec_xi.RData")
-load("cache/tab_sec_x.RData")
 load("cache/tab_vic2.RData")
+load("cache/input.l.Rdata")
 
+fls <- list.files("cache/")[str_detect(list.files("cache/"), "^tab_sec_")]
+for(file.nom in fls){
+  print(file.nom)
+  load( paste0("cache/", file.nom))
+}
 
+# input.l <- list()
 
 
 # TASA DE AGRESIÓN ----
+base_endireh <- tab_sec_x %>% 
+  group_by(SEXO) %>% 
+  summarise(base_mujeres = sum(FAC_MUJ))
+input.l$base_endireh <- base_endireh
 
-# 2015 (endireh)
 
-# tasa de agresión de 15 a 19 ----
-tab_tardia <- tab_sec_x %>% 
-  dplyr::select(starts_with("FAC_"), 
-                P10_1_3, 
-                SEXO, EDAD, edad_num) %>% 
-  mutate(edad_gpo = cut(edad_num, include.lowest = T,
-                        breaks = c(15, 19, 100)),
-         agresion = factor(P10_1_3, 
-                1:4, 
-                c("muchas veces", "pocas veces", "una vez", "no ocurrió")),
-         violacion = agresion != "no ocurrió")
+# 2016 (endireh)
 
-tab_tardia %>% 
-  group_by(edad_gpo, violacion) %>% 
-  summarise(n = n(), 
-            nfac = sum(FAC_MUJ)) %>% 
-  group_by(edad_gpo) %>% 
-  mutate(prop = nfac/sum(nfac))
-  
-tab_tardia %>% 
-  filter(violacion) %>% 
-  group_by(edad_gpo, agresion) %>% 
-  summarise(n = n(), 
-            nfac = sum(FAC_MUJ)) %>% 
-  group_by(edad_gpo) %>% 
-  mutate(prop = nfac/sum(nfac))
+#  Preguntas ----
 
-# infancia y adolescencia temprana menos de 15 ----
-tab_temp <- tab_sec_xi %>% 
-  dplyr::select(starts_with("FAC_"), 
-                P11_12_5, 
-                SEXO, EDAD, edad_num) %>% 
-  mutate(agresion = factor(P11_12_5, 
-                           1:2, 
-                           c("sí", "no")) )
-tab_temp %>% 
-  group_by(agresion) %>% 
-  summarise(n = n(), 
-            nfac = sum(FAC_MUJ)) %>% 
+# P6_6_15: Durante su vida de estudiante, ¿alguna persona de las escuelas a las que asistió a estudiar, (como maestra o maestro, director, prefecto, compañero de clase o alumno de la escuela u otro trabajador o persona de la escuela) la han obligado a tener relaciones sexuales en contra de su voluntad?
+# P7_9_13:	Dígame si en alguno de sus trabajos, ¿Alguna o algunas personas que trabajaban con usted	la han obligado a tener relaciones sexuales en contra de su voluntad?
+# P8_1_13:	¿Alguna vez la han obligado a tener relaciones sexuales en contra de su voluntad?
+# P10_1_3:	Durante el último año, de octubre de 2015 a la fecha, ¿alguna o algunas personas de su familia (no incluya a su pareja o esposo) la han obligado a tener relaciones sexuales en contra de su voluntad
+# P12_17_5: ¿Alguna de sus parejas o esposo(s) anteriores, ¿durante su relación o después de separarse la agredió sexualmente (intentó obligarla o la obligó a tener relaciones sexuales por la fuerza o con amenazas)?
+# P11_12_5:	Durante su infancia (hasta antes de cumplir 15 años), ¿dígame si vivió o le ocurrieron algunas de las situaciones que se mencionan en la tarjeta? ¿La obligaron a tener relaciones sexuales bajo amenazas o usando la fuerza?
+
+
+
+# Tabla con preguntas ----
+names(tab_sec_vi)
+tab_union <- tab_sec_vi %>% 
+  dplyr::select(ID_VIV:FAC_MUJ, edad_num, P6_6_15) %>% # nrow 111,256
+  left_join(tab_sec_vii %>% 
+              dplyr::select(ID_VIV:FAC_MUJ, edad_num, P7_9_13)) %>% 
+  left_join(tab_sec_viii %>% 
+              dplyr::select(ID_VIV:FAC_MUJ, edad_num, P8_1_13)) %>% 
+  left_join(tab_sec_x %>% 
+              dplyr::select(ID_VIV:FAC_MUJ, edad_num, P10_1_3)) %>% 
+  left_join(tab_sec_xii %>% 
+              dplyr::select(ID_VIV:FAC_MUJ, edad_num, P12_17_5)) %>% 
+  left_join(tab_sec_xi %>% 
+              dplyr::select(ID_VIV:FAC_MUJ, edad_num, P11_12_5)) %>% 
+  mutate(P10_1_3 = ifelse(parse_number(P10_1_3) %in% 1:3, 1, 2),
+         edad_gpo = cut(edad_num, include.lowest = T,
+                        breaks = c(15, 19, 100)) )
+dim(tab_union)
+tab_union %>% data.frame() %>% head()
+
+tab <- tab_union %>%
+  filter(edad_gpo == "[15,19]") %>% 
+  gather(pregunta, resp, P6_6_15:P11_12_5) %>%
+  mutate(pregunta = fct_recode(pregunta,
+                               `estudiante` = "P6_6_15",
+                               `trabajo` = "P7_9_13",
+                               `comunidad` = "P8_1_13",
+                               `familia` = "P10_1_3",
+                               `pareja` = "P12_17_5",
+                               `infancia` = "P11_12_5")) %>% 
+  group_by(pregunta, resp) %>% 
+  summarise(nfac = sum(FAC_MUJ),
+            n = n()) %>% 
+  group_by(pregunta, resp) %>% 
+  mutate(nfac_sin = sum((!is.na(resp))*nfac, na.rm = T)) %>% 
+  group_by(pregunta) %>% 
+  mutate(prop = 100*nfac/sum(nfac), 
+         prop_sin = 100*nfac/sum(nfac_sin),
+         base = sum(nfac),
+         base_sin = sum(nfac_sin)) %>% 
+  ungroup
+tab
+
+gg <- tab %>% 
+  filter(resp == 1,
+         pregunta != "infancia") %>% 
+  ggplot(aes(x = pregunta, 
+             y = prop_sin) )+ 
+  geom_bar(stat = "identity", position = "stack") + 
+  geom_label(aes(label = format(nfac, big.mark = ","))) + 
+  ylab("Proporción (%)") + 
+  xlab("Ambiente") + 
+  ggtitle("Proporción de mujeres respuesta sí por ambiente.",
+          "Mujeres entre 15 y 19 años.")
+gg 
+input.l$gg_agr_tard_endireh <- gg
+
+gg <- tab_union %>%
+  mutate(P11_12_5 = factor(P11_12_5, 
+                           c(1,2,8), 
+                           c("Sí", "No", "No recuerda"))) %>% 
+  group_by(P11_12_5) %>% 
+  summarise(nfac = sum(FAC_MUJ),
+            n = n()) %>% 
   ungroup %>% 
-  mutate(prop = nfac/sum(nfac))
-  
+  mutate(prop = 100*nfac/sum(nfac), 
+         base = sum(nfac)) %>% 
+  ungroup %>% 
+  filter(P11_12_5 != "No") %>% 
+  ggplot(aes(x = P11_12_5, 
+             y = prop) )+ 
+  geom_bar(stat = "identity", position = "stack") + 
+  geom_label(aes(label = format(nfac, big.mark = ","))) + 
+  ylab("Proporción (%)") + 
+  xlab("Respuesta") + 
+  ggtitle("Proporción de mujeres agredidas\nantes de cumplir 15 años.",
+          "Mujeres de mas de 15 años.")
+gg 
+input.l$gg_agr_temp_endireh <- gg
+
+# cache("input.l")
 
 
 # 2016 (envipe) ----
+
 # 18 años
 tab_envipe <- tab_vic2 %>% 
   filter(situacion == "14") %>% 
-  mutate(SEXO = factor(SEXO, 1:2, c("H", "M")), 
-         edad_grupo = cut_width(edad_num, width = 5))
+  mutate(SEXO = factor(SEXO, 1:2, c("H", "M")))
+
+input.l$base_sexo_envipe <- tab_envipe %>% 
+  group_by(SEXO) %>% 
+  summarise(base = sum( parse_number(FAC_ELE)))
+
 
 # tasa general
-tab_envipe %>% 
+tab <- tab_envipe %>% 
   group_by(ocurrió) %>% 
   summarise(n = n(), 
             nfac = sum(parse_number(FAC_ELE))) %>% 
   ungroup %>% 
-  mutate(prop = nfac/sum(nfac))
+  mutate(prop = nfac/sum(nfac), 
+         base = sum(nfac)) 
+tab
+input.l$tab_agr_gral_envipe <- tab
 
 # tasa por sexo
-tab_envipe %>% 
+tab <- tab_envipe %>% 
   group_by(ocurrió, SEXO) %>% 
   summarise(n = n(), 
             nfac = sum(parse_number(FAC_ELE))) %>% 
   group_by(ocurrió) %>% 
-  mutate(prop = nfac/sum(nfac))
+  mutate(prop_ocu = nfac/sum(nfac)) %>% 
+  group_by(SEXO) %>% 
+  mutate(prop_sex = nfac/sum(nfac)) %>% 
+  ungroup
+tab
+input.l$tab_agr_sexo_envipe <- tab
+
+gg <- tab %>% 
+  filter(ocurrió == 1) %>% 
+  ggplot(aes(x = SEXO, 
+             y = prop_ocu) )+ 
+  geom_bar(stat = "identity", position = "stack") + 
+  geom_label(aes(label = format(nfac, big.mark = ","))) + 
+  ylab("Proporción (%)") + 
+  xlab("Sexo") + 
+  ggtitle("Proporción de personas agredidas por sexo.",
+          "Hombres y mujeres mayores de 18 años.")
+gg
+input.l$gg_agr_sexo_envipe <- gg
+
 
 tab_envipe %>% 
   filter(ocurrió == 1) %>% 
@@ -88,24 +175,24 @@ tab_envipe %>%
   summarise(n = n(), 
             nfac = sum(parse_number(FAC_ELE)))
 
-tab_vic2 %>% 
-  mutate
-  filter(situacion == "14") %>% 
-  mutate()
-  group_by(ocurrió) %>% 
-  summarise(n = n(), 
-            nfac = sum(parse_number(FAC_ELE)))
+
 
 
 # 2007 (envin) ----
 # numero de mujeres 15 a 24 años
-(tab_mjov2$FAC_PER %>% sum) # 14 millones mujeres entre 15 y 24 años
+input.l$base_envin <- (tab_mjov2$FAC_PER %>% sum) # 14 millones mujeres entre 15 y 24 años
 
 # algunas vez obligaron
-tab_mjov2 %>% 
+tab <- tab_mjov2 %>% 
   group_by(P9_6) %>% 
   summarise(n = n(), 
-            nfac = sum(FAC_PER))
+            nfac = sum(FAC_PER)) %>% 
+  ungroup %>% 
+  na.omit %>% 
+  mutate(prop = 100*nfac/sum(nfac))
+tab
+input.l$tab_agr_muj_envin <- tab
+
 
 # varias veces
 tab_mjov2 %>% 
@@ -113,27 +200,48 @@ tab_mjov2 %>%
   summarise(n = n(), 
             nfac = sum(FAC_PER))
 
+
 # edad de primera vez obligaron
-tab_mjov2 %>%
+tab <- tab_mjov2 %>%
   mutate(edad_gpo = cut(parse_number(P9_9),
                         right = T,
                         breaks = c(0, 9, 14, 19, 100), 
                         include.lowest = T)) %>% 
   group_by(edad_gpo) %>% 
   summarise(n = n(), 
-            nfac = sum(FAC_PER))
+            nfac = sum(FAC_PER)) %>% 
+  ungroup %>% 
+  na.omit %>% 
+  mutate(prop = 100*nfac/sum(nfac))
+tab
+input.l$tab_agr_edad1era_envin <- tab
+
+
+gg <- ggplot(tab, aes(x = edad_gpo, 
+                y = prop)) + 
+  geom_bar(stat = "identity") + 
+  geom_label(aes(label = format(nfac, big.mark = ","))) + 
+  ylab("Proporción (%)") + 
+  xlab("Grupo de edad") + 
+  ggtitle("Distribución por edad de primera agresión.",
+          "Mujeres entre 15 y 24 años.")
+gg
+input.l$gg_agr_edad1era_envin <- gg
+
 
 # alguna vez embarazo
-tab_mjov2 %>% 
+tab <- tab_mjov2 %>% 
   mutate(edad_gpo = cut(parse_number(P9_9),
                                     right = T,
                                     breaks = c(0, 9, 14, 19, 100), 
                                     include.lowest = T)) %>% 
-  group_by(edad_gpo, P9_12) %>% 
+  group_by(P9_12) %>% 
   summarise(n = n(), 
-            nfac = sum(FAC_PER)) 
+            nfac = sum(FAC_PER)) %>% 
+  ungroup() %>% 
+  na.omit() %>% 
+  mutate(prop = 100*nfac/sum(nfac))
+tab
+input.l$tab_agr_emb_envin <- tab
 
-# embarazo sólo una persona 
-tab_mjov2 %>% 
-  filter(P9_12 == 1) %>% 
-  data.frame()
+cache("input.l")
