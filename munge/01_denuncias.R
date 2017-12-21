ab29c8bd995b3ce57979a2e0f7998f6b021154d3
Songeo <sonia.gmcl@gmail.com>
2017-12-21 17:28:43 -0600

    preview denuncias

---
+++ munge/01_denuncias.R
@@ -0,0 +1,17 @@
+
+# library(tidyverse)
+# library(foreign)
+# 
+# list.files("data/censo_trib_superior/")
+# 
+# dat.vic <- read.dbf("data/censo_trib_superior/AP_iniciadas_CI_abiertas_cnpje2016_dbf/Bases_datos/VICTIMAS.DBF") %>% 
+#   as.tibble() %>% 
+#   left_join(read.dbf("data/censo_trib_superior/AP_iniciadas_CI_abiertas_cnpje2016_dbf/Catalogos/DELI_FC.DBF"), 
+#             by = "DELI_FC") %>% 
+#   left_join(read.dbf("data/censo_trib_superior/AP_iniciadas_CI_abiertas_cnpje2016_dbf/Catalogos/RAN_EDAD.DBF"),
+#             by = "RAN_EDAD") %>% 
+#   mutate(tipo_del = parse_number(str_sub(DELI_FC, 1, 2)),
+#          desc_delito = factor(str_trim(DESC_TIP)), 
+#          edad = factor(str_trim( str_replace_all(DESCRIP, "a\xa4os", ""))),
+#          sexo_rec = factor(SEXO, levels = c(1,2), labels = c("Hom", "Muj")))
+# dat.vic %>% data.frame %>% head
