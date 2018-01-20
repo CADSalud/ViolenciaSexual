b112c9658327286d6b71fd4497f5b22e38c292ad
Songeo <sonia.gmcl@gmail.com>
2018-01-19 22:28:42 -0600

    boot envipe 2012 a 2016

--- munge/01_envipe.R
+++ munge/01_envipe.R
@@ -44,10 +44,14 @@ cache("tab_demos_envipe")
 
 files <- list.files("data/envipe/")
 files.bd <- files[str_detect(files, "^bd_envipe")]
+
 df_vic2_envipe <- lapply(files.bd, function(file.nom){
   print(file.nom)
+  nom.vic <- "TPer_Vic2.dbf"
+  if(file.nom == "bd_envipe2013_dbf"){nom.vic <- "tper_vic.dbf"}
+  print(nom.vic)
   
-  read.dbf(paste0("data/envipe/", file.nom, "/TPer_Vic2.dbf")) %>% 
+  read.dbf(paste0("data/envipe/", file.nom, "/", nom.vic)) %>% 
     as_tibble() %>% 
     dplyr::select(UPM, VIV_SEL, HOGAR, R_SEL,
                   FAC_ELE,
@@ -61,7 +65,7 @@ df_vic2_envipe <- lapply(files.bd, function(file.nom){
     separate(preg, c('seccion', 'preg_num', 'situacion'), sep = "_") %>% 
     mutate(preg_num = factor(preg_num, 3:4, c("ocurrió", "num_veces"))) %>% 
     spread(preg_num, value) %>% # 1 = Sí
-    mutate(edad_num = parse_number(EDAD), 
+    mutate(edad_num = parse_number(as.character(EDAD)), 
            FAC_ELE = parse_number(FAC_ELE), 
            file.nom = file.nom, 
            year = parse_number(file.nom)-1)
@@ -70,13 +74,22 @@ df_vic2_envipe <- lapply(files.bd, function(file.nom){
 
 df_vic2_envipe
 df_vic2_envipe %>% summary
+df_vic2_envipe$year %>% summary
 
 
 cache("df_vic2_envipe")
-write.csv(df_vic2_envipe, "doc/df_vic2_envipe.csv", row.names = F)
-
+# df_vic2_envipe %>% 
+#   filter(year == 2012) %>% 
+#   write.csv("doc/df_vic2_2012.csv", row.names = F)
+# gsutil cp df_vic2_2012.csv gs://envipe
 
 df_vic2_envipe %>% 
-  filter(year == 2016) %>% 
-  write.csv("doc/df_vic2_envipe_2016.csv", row.names = F)
-
+  mutate(edad_n = parse_number(as.character(EDAD))) %>% 
+  filter(edad_n > 39, 
+         ocurrió == 1,
+         situacion == 14) %>% 
+  group_by(year, edad_n) %>% 
+  tally() %>% 
+  spread(edad_n, n) %>% 
+  data.frame()
+    
---
+++ src/02_boot_envipe.R
@@ -0,0 +1,152 @@
+
+
+library(ProjectTemplate)
+load.project()
+
+load("cache/poblacion.RData")
+
+library(bigrquery)
+
+
+# Proyecto ----
+project <- "acoustic-field-186719" # put your project ID here
+
+df_envipe <- lapply(2012:2016, function(year.nom){
+  print(year.nom)
+  sql <- paste0("SELECT year, SEXO, edad_num, seccion, ",
+                "situacion, ocurri__, SUM(FAC_ELE), COUNT(year) ", 
+                "FROM [acoustic-field-186719:envipe.vic2_", 
+                year.nom,
+                "] WHERE situacion = 14 ", # violacion sexual
+                "GROUP BY year, SEXO, edad_num, seccion, situacion, ocurri__")
+  tt <- query_exec(sql, project = project) 
+  }) %>% 
+  bind_rows() %>% 
+  mutate(edad_gpo = cut(edad_num, breaks = c(seq(19,65, by = 5), 97), 
+                        include.lowest = F)) %>% 
+  dplyr::rename(nfac = f0_, n = f1_, ocurrencia = ocurri__) %>% 
+  as_tibble()
+
+df_envipe
+df_envipe %>% 
+  group_by(edad_gpo) %>% 
+  tally
+  
+query_exec("select * FROM [acoustic-field-186719:envipe.vic2_2012] limit 3", 
+           project = project) 
+
+df_envipe_id <- lapply(2012:2016, function(year.nom){
+  print(year.nom)
+  sql <- paste0("SELECT UPM, VIV_SEL, HOGAR, R_SEL, FAC_ELE, ",
+                "edad_num, year, situacion, ocurri__ ",
+                "FROM [acoustic-field-186719:envipe.vic2_", 
+                year.nom,
+                "] WHERE situacion = 14 and SEXO = 2")
+  tt <- query_exec(sql, project = project) %>% 
+    as_tibble()
+}) %>% 
+  bind_rows() %>% 
+  rename(ocurrencia = ocurri__) %>% 
+  unite(id_persona, c('UPM', 'VIV_SEL', 'HOGAR', 'R_SEL')) %>% 
+  mutate(edad_gpo = cut(edad_num, breaks = c(seq(19,65, by = 5), 97), 
+                        include.lowest = F))
+df_envipe_id
+
+
+# tablas necesarias ----
+tab_envipe <- df_envipe %>% 
+  filter(SEXO == 2) %>% 
+  group_by(edad_gpo, year, ocurrencia) %>% 
+  summarise(nfac = sum(nfac)) %>% 
+  group_by(edad_gpo, year) %>% 
+  mutate(pob_envipe = sum(nfac)) %>% 
+  ungroup
+  
+tab_envipe
+
+tab_envipe %>% 
+  filter(ocurrencia == 1, 
+         !is.na(edad_gpo)) %>% 
+  ggplot(aes(x = edad_gpo, y = nfac, 
+             color = factor(year), 
+             group = year)) + 
+  geom_point() + 
+  geom_line()
+
+poblacion$edad %>% summary()
+tab_conapo <- poblacion %>% 
+  filter(edad > 19, 
+         anio > 2011) %>% 
+  mutate(edad_gpo = cut(edad, breaks = c(seq(19,65, by = 5), 97), 
+                        include.lowest = F), 
+         year = parse_number(anio)) %>% 
+  filter(sexo == "Mujeres") %>% 
+  group_by(sexo, edad_gpo, year) %>% 
+  summarise(pob_conapo = sum(pob)) %>% 
+  ungroup()
+tab_conapo
+
+tab_envipe %>% head
+tab_conapo %>% head
+
+
+
+# proporcion de abuso
+tab <- tab_envipe %>% 
+  left_join(tab_conapo, 
+            by = c("edad_gpo", "year")) %>% 
+  na.omit() %>% 
+  mutate(prop_envipe = nfac/pob_envipe, 
+         prop_conapo = nfac/pob_conapo)
+tab
+
+tab %>% 
+  filter(ocurrencia == 1) %>% 
+  ggplot(aes(x = prop_envipe, y = prop_conapo, 
+             color = factor(year))) + 
+  geom_point() + 
+  geom_abline(slope = 1, intercept = 0) +
+  scale_x_continuous(labels = function(x)100000*x) +
+  scale_y_continuous(labels = function(x)100000*x) +
+  facet_wrap(~edad_gpo, scales = "free")
+
+# Remuestreo----
+
+prop_fun <- function(sub){
+  sub %>% 
+    group_by(year, edad_gpo, ocurrencia) %>% 
+    summarise(n_fac = sum(FAC_ELE)) %>% 
+    group_by(year, edad_gpo) %>% 
+    mutate(prop_envipe = n_fac/sum(n_fac)) %>% 
+    ungroup() %>% 
+    left_join(tab_conapo, 
+              by = c("year", "edad_gpo")) %>% 
+    mutate(prop_conapo = n_fac/pob_conapo)
+}
+
+
+tab <- df_envipe_id %>% 
+  filter(!is.na(edad_gpo), 
+         edad_num <= 54) %>% 
+  group_by(year) %>% 
+  bootstrap(m = 100, by_group = T) %>% 
+  do(prop_fun(.)) %>% 
+  ungroup
+tab
+
+tab %>% 
+  filter(ocurrencia == 1) %>% 
+  ggplot(aes(x = edad_gpo, 
+             y = prop_envipe*1e5,
+             fill = factor(year))) + 
+  geom_boxplot()
+
+tab %>% 
+  filter(ocurrencia == 1) %>% 
+  ggplot(aes(x = edad_gpo, 
+             y = prop_conapo*1e5,
+             fill = factor(year))) + 
+  geom_boxplot()
+
+
+
